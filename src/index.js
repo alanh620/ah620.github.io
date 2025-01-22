086bbe5a9fda8771d21dbf37ab627bec36c3ae5d
alanh620 <alan.hasler@thedigitalring.com>
2025-01-22 06:53:06 -0600

    fixed?

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css'
 import './App.scss'
+import Home from './pages';
 
 ReactDOM.render(
 <React.StrictMode>
 	<App />
+	<Home />
 </React.StrictMode>,
 document.getElementById('root')
 );
